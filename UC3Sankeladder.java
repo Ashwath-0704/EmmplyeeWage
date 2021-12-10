12fd829e660146cb3081592d8d109c5899aaccc0
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-10 11:37:18 +0530

    UC3Sankeladder

---
+++ UC3Sankeladder.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class UC3Sankeladder {
+
+	public static void main(String[] args) {
+		
+		int diceRoll = 0;
+		int n;  
+        Scanner sc=new Scanner(System.in); 
+        System.out.print("Enter the Nos of snakes you want : ");  
+        n=sc.nextInt(); 
+        int [] snakeArr = new int [n];//5
+		System.out.print("Enter the numbers : "); 
+        for (int a=0;a<snakeArr.length;a++) {
+            snakeArr[a]=sc.nextInt();// {10 20 55 94 66}
+        }
+		int f;  
+        Scanner sc1=new Scanner(System.in); 
+        System.out.print("Enter the Nos of Lader you want : ");  
+        f=sc1.nextInt(); 
+        int [] LaderArr = new int [f];//5
+		System.out.print("Enter the numbers : "); 
+        for (int a=0;a<LaderArr.length;a++) {
+            LaderArr[a]=sc1.nextInt();// {10 20 55 94 66}
+        }
+		
+
+		System.out.println("Starting Position of the Player = 0");
+		
+		for (int plyerPosition = 1; plyerPosition <= 100; plyerPosition++) {
+			double randomnumber = (double) (Math.random() * 6);
+			diceRoll = (int) randomnumber;
+			
+			System.out.println("Dice Number =" + " " + diceRoll);
+			
+			plyerPosition = plyerPosition + diceRoll;
+			plyerPosition = plyerPosition - 1;
+			
+			System.out.println("Current Position =" + " " + plyerPosition);
+			
+			if (diceRoll == 0) {
+				// plyerPosition = plyerPosition - diceRoll;
+				System.out.println("You have to stay on the same position!!");
+			}
+			if (plyerPosition == snakeArr[0]) {
+				plyerPosition = 5;
+				System.out.println("IT'S A SNAKE \n, Current Position is :  " + plyerPosition);
+			} else if (plyerPosition == snakeArr[1]) {
+				plyerPosition = 48;
+				System.out.println("IT'S A SNAKE \n, Current Position is :  " + plyerPosition);
+			} else if (plyerPosition == snakeArr[2]) {
+				plyerPosition = 77;
+				System.out.println("IT'S A SNAKE \n, Current Position is :  " + plyerPosition);
+			} else if (plyerPosition == LaderArr[0]) {
+				plyerPosition = 34;
+				System.out.println("YOU GOT A LADDER,\n Current Position is : " + plyerPosition + " position.");
+			} else if (plyerPosition == LaderArr[1]) {
+				plyerPosition = 64;
+				System.out.println("YOU GOT A LADDER,\n Current Position is : " + plyerPosition + " position.");
+			} else if (plyerPosition == LaderArr[2]) {
+				plyerPosition = 86;
+				System.out.println("YOU GOT A LADDER,\n Current Position is : " + plyerPosition + " position.");
+			} else if (plyerPosition > 100) {
+				plyerPosition = plyerPosition - diceRoll;
+				System.out.println("YOU CAN'T JUMP, YOU MUST LAND ON A 100.");
+			}
+		}
+	}
+}
