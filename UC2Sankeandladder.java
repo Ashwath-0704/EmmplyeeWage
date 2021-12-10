cc2b16b121f9109897296e5486c72bb30a02833a
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-10 11:36:20 +0530

    UC2Sankeladder

---
+++ UC2Sankeandladder.java
@@ -0,0 +1,13 @@
+public class UC2Sankeandladder {
+
+	public static void main(String[] args) {
+		
+		int P = 0;
+		int dice = 0;
+
+		System.out.println("Current Position of the Player = " + P);
+				double randomnumber = (double) (Math.random() * 6) + 1;
+				dice = (int) randomnumber;
+				System.out.println("Dice Number =" + " " + dice);
+	}
+}
